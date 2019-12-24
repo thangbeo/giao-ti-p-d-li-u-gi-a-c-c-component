d06f069bc6e5fc3a902cae2237e2211205862ca2
thangbeo <38211590+thangbeo@users.noreply.github.com>
2019-12-24 21:06:37 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld title="qqweqweq" msg="Welcome to Your Vue.js App" @sendback="logCallBack"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    logCallBack(msgValue){
+      alert(msgValue)
+    }
+  }
+
+
+  
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
